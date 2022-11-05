6799a18b01bdfdc656e2e4e98827fac6591d8f13
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-11-05 09:07:32 +0700

    Fairpool

--- models/Person/spreadName.ts
+++ models/Person/spreadName.ts
@@ -1,10 +1,13 @@
+import { ensure } from 'zenbox-util/ensure'
 import { fromStringToId } from '../Id'
 
 export function spreadName(firstName?: string, lastName?: string, nickname?: string, uid?: string) {
-  const name = (firstName || lastName) ? `${firstName ?? ''} ${lastName ?? ''}`.trim() : undefined
+  const $name = (firstName || lastName) ? `${firstName ?? ''} ${lastName ?? ''}`.trim() : undefined
+  const $nickname = nickname || firstName
+  const $uid = ensure(uid || ($name && fromStringToId($name)) || (nickname && fromStringToId(nickname)), new Error('Person.uid is required'))
   return {
-    uid: uid || (name && fromStringToId(name)) || (nickname && fromStringToId(nickname)),
-    name: name,
-    nickname: nickname || firstName,
+    uid: $uid,
+    name: $name,
+    nickname: $nickname,
   }
 }
