9c0c45215ae94c601b73e379030a3448997eddc2
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-13 08:28:02 +0700

    Fairpool

---
+++ models/Idx.ts
@@ -0,0 +1,16 @@
+import { isEqualSC } from 'libs/utils/lodash'
+import { getArraySchema } from 'libs/utils/zod'
+import { identity } from 'lodash-es'
+import { z } from 'zod'
+
+export const IdxSchema = z.number().nonnegative().describe('Idx')
+
+export const IdxesSchema = getArraySchema(IdxSchema, identity)
+
+export type Idx = z.infer<typeof IdxSchema>
+
+export const parseIdx = (idx: Idx): Idx => IdxSchema.parse(idx)
+
+export const parseIdxes = (s: Idx[]): Idx[] => IdxesSchema.parse(s)
+
+export const isEqualIdx = isEqualSC
---
+++ models/Ordinal.ts
@@ -0,0 +1,16 @@
+import { getArraySchema } from 'libs/utils/zod'
+import { identity } from 'lodash-es'
+import { z } from 'zod'
+import { isEqualSC } from '../../utils/lodash'
+
+export const OrdinalSchema = z.number().positive().describe('Ordinal')
+
+export const OrdinalsSchema = getArraySchema(OrdinalSchema, identity)
+
+export type Ordinal = z.infer<typeof OrdinalSchema>
+
+export const parseOrdinal = (ordinal: Ordinal): Ordinal => OrdinalSchema.parse(ordinal)
+
+export const parseOrdinals = (s: Ordinal[]): Ordinal[] => OrdinalsSchema.parse(s)
+
+export const isEqualOrdinal = isEqualSC
