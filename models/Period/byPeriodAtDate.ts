a9b02f65f0f5d5f1d1cabe377e3e6f25a3bd9c00
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-04-01 14:15:02 +0700

    WIP: writeTaxReport

--- models/Period/byPeriodAtDate.ts
+++ models/Period/byPeriodAtDate.ts
@@ -3,3 +3,7 @@ import { Period } from '../Period'
 export const byPeriodAtDate = (period: Period) => (object: { date: Date }) => {
   return object.date >= period.from && object.date < period.to
 }
+
+export const byPeriod = (period: Period) => (object: Period) => {
+  return object.from >= period.from && object.to <= period.to
+}
