4687a0d08bf9401946622df52522cc732377fd09
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-12 12:57:43 +0700

    CostProof

--- models/NaturalNumber.ts
+++ models/NaturalNumber.ts
@@ -1,5 +1,15 @@
+import { getArraySchema } from 'libs/utils/zod'
+import { equals, identity } from 'remeda'
 import { z } from 'zod'
 
-export const NaturalNumberSchema = z.number().int().nonnegative()
+export const NaturalNumberSchema = z.number().int().nonnegative().describe('NaturalNumber')
+
+export const NaturalNumbersSchema = getArraySchema(NaturalNumberSchema, identity)
 
 export type NaturalNumber = z.infer<typeof NaturalNumberSchema>
+
+export const parseNaturalNumber = (num: NaturalNumber): NaturalNumber => NaturalNumberSchema.parse(num)
+
+export const parseNaturalNumbers = (nums: NaturalNumber[]): NaturalNumber[] => NaturalNumbersSchema.parse(nums)
+
+export const isEqualNaturalNumber = equals
